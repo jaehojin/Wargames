363b760ac644c0221894ecd8d8f68cc7748e919f
root <root@DESKTOP-5UJH6B8>
2022-10-10 02:14:08 +0900

    Interpret DH_Testify

---
+++ DH_Testify/chal.py
@@ -0,0 +1,18 @@
+from pwn import *
+
+#context.log_level = "debug"
+
+LOCAL = False
+if LOCAL:
+    p = process("./chal")
+else:
+    PORT = int(input("PORT: "))
+    HOST = int(input("HOST: "))
+    p = remote("host"+str(HOST)+".dreamhack.games", PORT)
+
+
+def slog(name, addr):
+    return success(name + ": " + str(hex(addr)))
+
+
+p.interactive()
